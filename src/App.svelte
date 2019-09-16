27801b4a2a85f51d77b61f19a7e93db71b141380
anfilat <anfilat@mail.ru>
2019-09-16 16:22:48 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	let lastKey;
+
+	function onKeyPress(event) {
+		lastKey = event.key;
+	}
+
+	function onClick() {
+		//console.log('click');
+	}
+</script>
+
+<style>
+	.main {
+		min-height: 100vh;
+	}
+
+	.lastKey {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		font-size: 64px;
+		font-family: monospace;
+	}
+</style>
+
+<svelte:body on:keypress={onKeyPress} />
+
+<div class="main" on:click={onClick}>
+	{#if lastKey}
+		<div class="lastKey">{lastKey}</div>
+	{/if}
+</div>
