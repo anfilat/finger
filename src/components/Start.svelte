00465b6bda4bd930e5c77b71f16f9f957ffde043
anfilat <anfilat@mail.ru>
2019-12-25 18:37:45 +0300

    Немного подравнял верстку

--- src/components/Start.svelte
+++ src/components/Start.svelte
@@ -8,7 +8,11 @@
 <svelte:body on:click="{() => setupMode.set(true)}" />
 
 {#if $setupMode}
-    <button class="button is-primary is-large" on:click|stopPropagation="{() => setupMode.set(false)}" disabled={!$setupOk}>
+    <button
+        class="button is-primary is-large block"
+        on:click|stopPropagation="{() => setupMode.set(false)}"
+        disabled={!$setupOk}
+    >
         Start
     </button>
 {/if}
--- src/components/TrainingType.svelte
+++ src/components/TrainingType.svelte
@@ -6,8 +6,10 @@
 	export let trainingType;
 </script>
 
-Select training
-<div class="control">
+<div class="block">
+	Select training
+</div>
+<div class="control block">
 	<label class="radio">
 		<input type=radio bind:group={$trainingType} value={OneKeyTraining}>
 		One Key
