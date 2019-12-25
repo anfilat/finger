2d8d3030bf70ee9c790b76954b8ffca3af1e7e48
anfilat <anfilat@mail.ru>
2019-12-25 11:12:01 +0300

    Другой плагин для подключения css

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss'
+import css from 'rollup-plugin-css-only'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -26,7 +26,7 @@ export default {
 			}
 		}),
 
-		postcss(),
+		css ({output: "public/bulma.css"}),
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration —
