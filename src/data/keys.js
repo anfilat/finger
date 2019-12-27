0fc92c7591196cbbfc1a043edbe3c55a0a6eb19a
anfilat <anfilat@mail.ru>
2019-12-27 12:47:11 +0300

    Рефакторинг списка букв

--- src/data/keys.js
+++ src/data/keys.js
@@ -1,103 +1,95 @@
-const keys = [
-	{key: 'ё', base: 'ф'},
-	{key: '1', base: 'ф'},
-	{key: '2', base: 'ф'},
-	{key: '3', base: 'ы'},
-	{key: '4', base: 'в'},
-	{key: '5', base: 'а'},
-	{key: '6', base: 'а'},
-	{key: '7', base: 'о'},
-	{key: '8', base: 'л'},
-	{key: '9', base: 'д'},
-	{key: '0', base: 'ж'},
-	{key: '-', base: 'ж'},
-	{key: '=', base: 'ж'},
-	{key: '\\', base: 'ж'},
-
-	{key: '!', base: 'ф'},
-	{key: '"', base: 'ф'},
-	{key: '№', base: 'ы'},
-	{key: ';', base: 'в'},
-	{key: '%', base: 'а'},
-	{key: ':', base: 'а'},
-	{key: '?', base: 'о'},
-	{key: '*', base: 'л'},
-	{key: '(', base: 'д'},
-	{key: ')', base: 'ж'},
-	{key: '_', base: 'ж'},
-	{key: '+', base: 'ж'},
-	{key: '/', base: 'ж'},
-
-	{key: 'й', base: 'ф'},
-	{key: 'ц', base: 'ы'},
-	{key: 'у', base: 'в'},
-	{key: 'к', base: 'а'},
-	{key: 'е', base: 'а'},
-	{key: 'н', base: 'о'},
-	{key: 'г', base: 'о'},
-	{key: 'ш', base: 'л'},
-	{key: 'щ', base: 'д'},
-	{key: 'з', base: 'ж'},
-	{key: 'х', base: 'ж'},
-	{key: 'ъ', base: 'ж'},
-
-	{key: 'ф'},
-	{key: 'ы'},
-	{key: 'в'},
-	{key: 'а'},
-	{key: 'п', base: 'а'},
-	{key: 'р', base: 'о'},
-	{key: 'о'},
-	{key: 'л'},
-	{key: 'д'},
-	{key: 'ж'},
-	{key: 'э', base: 'ж'},
-
-	{key: 'я', base: 'ф'},
-	{key: 'ч', base: 'ы'},
-	{key: 'с', base: 'в'},
-	{key: 'м', base: 'а'},
-	{key: 'и', base: 'а'},
-	{key: 'т', base: 'о'},
-	{key: 'ь', base: 'о'},
-	{key: 'б', base: 'л'},
-	{key: 'ю', base: 'д'},
-	{key: '.', base: 'ж'},
-	{key: ',', base: 'ж'},
-];
-
-export const leftHand = ['ф', 'ы', 'в', 'а'];
-export const rightHand = ['о', 'л', 'д', 'ж'];
-
-export const hands = {
-	ф: leftHand,
-	ы: leftHand,
-	в: leftHand,
-	а: leftHand,
-	о: rightHand,
-	л: rightHand,
-	д: rightHand,
-	ж: rightHand,
-};
-
 export function isKey(value) {
 	return keys.some(({key}) => key == value);
 }
 
 export function getFromSelectedKeys(selected) {
-	const key = selected[Math.floor(Math.random() * selected.length)];
+	const key = getRandomItem(selected);
 	const keyData = keys.find(data => data.key === key);
 	return [keyData.key, getHandBase(keyData)];
 }
 
 export function getRandomKeys() {
-	const keyData = keys[Math.floor(Math.random() * keys.length)];
+	const keyData = getRandomItem(keys);
 	return [keyData.key, getHandBase(keyData)];
 }
 
 function getHandBase(keyData) {
-	if (keyData.base) {
-		return hands[keyData.base][Math.floor(Math.random() * 4)];
-	}
-	return null;
+	return keyData.base
+		? getRandomItem(keyData.base)
+		: null;
 }
+
+function getRandomItem(items) {
+	return items[Math.floor(Math.random() * items.length)];
+}
+
+const leftHand = 'фыва';
+const rightHand = 'олдж';
+
+const keys = [
+	{key: 'ё', base: leftHand},
+	{key: '1', base: leftHand},
+	{key: '2', base: leftHand},
+	{key: '3', base: leftHand},
+	{key: '4', base: leftHand},
+	{key: '5', base: leftHand},
+	{key: '6', base: leftHand},
+	{key: '7', base: rightHand},
+	{key: '8', base: rightHand},
+	{key: '9', base: rightHand},
+	{key: '0', base: rightHand},
+	{key: '-', base: rightHand},
+	{key: '=', base: rightHand},
+	{key: '\\', base: rightHand},
+
+	{key: '!', base: leftHand},
+	{key: '"', base: leftHand},
+	{key: '№', base: leftHand},
+	{key: ';', base: leftHand},
+	{key: '%', base: leftHand},
+	{key: ':', base: leftHand},
+	{key: '?', base: rightHand},
+	{key: '*', base: rightHand},
+	{key: '(', base: rightHand},
+	{key: ')', base: rightHand},
+	{key: '_', base: rightHand},
+	{key: '+', base: rightHand},
+	{key: '/', base: rightHand},
+
+	{key: 'й', base: leftHand},
+	{key: 'ц', base: leftHand},
+	{key: 'у', base: leftHand},
+	{key: 'к', base: leftHand},
+	{key: 'е', base: leftHand},
+	{key: 'н', base: rightHand},
+	{key: 'г', base: rightHand},
+	{key: 'ш', base: rightHand},
+	{key: 'щ', base: rightHand},
+	{key: 'з', base: rightHand},
+	{key: 'х', base: rightHand},
+	{key: 'ъ', base: rightHand},
+
+	{key: 'ф'},
+	{key: 'ы'},
+	{key: 'в'},
+	{key: 'а'},
+	{key: 'п', base: leftHand},
+	{key: 'р', base: rightHand},
+	{key: 'о'},
+	{key: 'л'},
+	{key: 'д'},
+	{key: 'ж'},
+	{key: 'э', base: rightHand},
+
+	{key: 'я', base: leftHand},
+	{key: 'ч', base: leftHand},
+	{key: 'с', base: leftHand},
+	{key: 'м', base: leftHand},
+	{key: 'и', base: leftHand},
+	{key: 'т', base: rightHand},
+	{key: 'ь', base: rightHand},
+	{key: 'б', base: rightHand},
+	{key: 'ю', base: rightHand},
+	{key: '.', base: rightHand},
+	{key: ',', base: rightHand},
+];
