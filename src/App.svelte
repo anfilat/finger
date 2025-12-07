dbf8603e563964ff85c4b0756f8cb69df5db7d70
Andrey Filatkin <anfilat@mail.ru>
2025-12-07 11:41:45 +0300

    init Svelte 5 version

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  // Главный компонент приложения
+</script>
+
+<main>
+  <h1>Finger Typing Tutor</h1>
+</main>
+
+<style>
+  :global(body) {
+    margin: 0;
+    font-family: system-ui, -apple-system, sans-serif;
+  }
+</style>
